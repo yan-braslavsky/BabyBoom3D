1525a7395c6019f9e82dfc3cdc4e452eccf07576
Yan Braslavskiy <iveliusster@gmail.com>
2013-11-22 13:15:30 +0200

    Day 3 Commit

--- Assets/Scripts/Object Manipulation/DoubleDoorInteraction.cs
+++ Assets/Scripts/Object Manipulation/DoubleDoorInteraction.cs
@@ -1,30 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using BB3D;
 
 public class DoubleDoorInteraction : MonoBehaviour
 {
-
-
 		private static string OPEN_LEFT_DOOR_ANIM_NAME = "OpenLeftDoor";
 		private static string OPEN_RIGHT_DOOR_ANIM_NAME = "OpenRightDoor";
-
 		public string RightDoorName;
 		public string LeftDoorName;
-
 		private bool leftDoorOpen;
 		private bool rightDoorOpen;
 
 		// Use this for initialization
 		void Start ()
 		{
-				//animation.GetClip(OPEN_LEFT_DOOR_ANIM_NAME).wrapMode = WrapMode.Once;
-
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-
 				if (Input.touchCount == 1) {
 						Touch touch = Input.touches [0];
 						Ray ray = Camera.main.ScreenPointToRay (Input.touches [0].position);
@@ -32,12 +26,20 @@ public class DoubleDoorInteraction : MonoBehaviour
 					
 						if (touch.phase == TouchPhase.Began && Physics.Raycast (ray.origin, ray.direction, out hit)) {
 
-								if (hit.collider.name.Equals (LeftDoorName) && !leftDoorOpen) {
-										//do something for this object
-										OpenLeftDoor ();
-								} else if (hit.collider.name.Equals (RightDoorName) && !rightDoorOpen) {
-										//do something for this object
-										OpenRightDoor ();
+								if (hit.collider.name.Equals (LeftDoorName)) {
+
+										if (!leftDoorOpen) {
+												OpenLeftDoor ();
+										} else { 
+												CloseLeftDoor ();
+										}
+								} else if (hit.collider.name.Equals (RightDoorName)) {
+
+										if (!rightDoorOpen) {
+												OpenRightDoor ();
+										} else { 
+												CloseRightDoor ();
+										}
 
 								}
 						}
@@ -47,19 +49,38 @@ public class DoubleDoorInteraction : MonoBehaviour
 
 		public void OpenLeftDoor ()
 		{
-				PlayAnimation (OPEN_LEFT_DOOR_ANIM_NAME);
+				PlayAnimation (OPEN_LEFT_DOOR_ANIM_NAME, false);
 				leftDoorOpen = true;
 		}
 
 		public void OpenRightDoor ()
 		{
-				PlayAnimation (OPEN_RIGHT_DOOR_ANIM_NAME);
+				
+				PlayAnimation (OPEN_RIGHT_DOOR_ANIM_NAME, false);
 				rightDoorOpen = true;
 		}
 
-		private void PlayAnimation (string animationName)
+		public void CloseLeftDoor ()
 		{
-				animation.Play (animationName);
+				PlayAnimation (OPEN_LEFT_DOOR_ANIM_NAME, true);
+				leftDoorOpen = false;
 		}
+		
+		public void CloseRightDoor ()
+		{
+				PlayAnimation (OPEN_RIGHT_DOOR_ANIM_NAME, true);
+				rightDoorOpen = false;
+		}
+
+		private void PlayAnimation (string animationName, bool reversed)
+		{
+				if (reversed) {
+						AnimationUtils.PlayAnimationReversed (animation, animationName);
+				} else {
+						AnimationUtils.PlayAnimation (animation, animationName);	
+				}
+
+		}
+
 
 }
